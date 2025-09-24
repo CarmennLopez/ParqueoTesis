13fad9560f2154db127f0fc84489e4a81db91307
Carmen <azucenaoscal@gmail.com>
2025-09-23 23:51:58 -0600

    base de datos actualizada

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 // server.js
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./src/routes/authRoutes'); // Importar las rutas de autenticación
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGODB_URI;
 
-// Aquí está la cadena de conexión del .env
-const mongoURI = process.env.MONGODB_URI; 
+// Middleware para parsear el cuerpo de las peticiones JSON
+app.use(express.json());
 
-mongoose.connect(mongoURI) // <--- ¡Código más limpio sin las opciones deprecadas!
+// Conexión a la base de datos
+mongoose.connect(mongoURI)
   .then(() => {
     console.log('✅ Conectado a la base de datos de MongoDB');
   })
   .catch(err => {
     console.error('❌ Error de conexión a la base de datos:', err);
   });
+
+// Usar las rutas de autenticación
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('¡API de parqueo funcionando!');
